6ea43157ed70241f0dedc8670b2b2c217ae2b746
FIRST_NAME LAST_NAME <l.frz65@gmail.com>
2024-06-02 04:25:23 +0200

    push xp

--- src/data/index.js
+++ src/data/index.js
@@ -1,8 +1,4 @@
-import {
-  algorithms,
-  devnotes,
-  oscs,
-} from "../assets";
+import { algorithms, devnotes, oscs } from "../assets";
 
 export const navLinks = [
   {
@@ -25,43 +21,32 @@ export const navLinks = [
 
 const experiences = [
   {
-    title: "YouTube Content Creator",
-    company_name: "Self Employed",
-    date: "2016 - Present",
+    title: "Developer Full stack",
+    company_name: "Student",
+    date: "2022 - 2024",
     details: [
-      "Built a subscriber base of over <span style='color: white;'>500,000 subscribers</span> by creating video content to help programmers.",
-      "Crafted visually appealling programming videos that have garnered over <span style='color: white;'>30,000,000 views</span>.",
-      "Produced high-quality educational and entertaining videos for clients including <span style='color: white;'>Intel, JetBrains, and MicroCenter</span>.",
-    ],
-  },
-  {
-    title: "Software Developer",
-    company_name: "Indie",
-    date: "2019 - 2023",
-    details: [
-      "Developed and delivered custom interdisciplinary coding portfolio for clients including <span style='color: white;'>Nvidia, Hostinger, and Amazon</span>.",
+      "Learning by mylself coding with numerous tutorial and create many online website ",
       "<span style='color: white;'>Designed and developed innovative</span> AI applications and interactive websites.",
       "<span style='color: white;'>Managed full project lifecycle</span> from concept to deployment in successful and timely project completions.",
     ],
   },
   {
-    title: "Software Engineer",
-    company_name: "Prime 3",
-    date: "2018 - 2019",
+    title: "Self Employment",
+    company_name: "Myself",
+    date: "2021 - 2022",
     details: [
-      "Built custom enterprise applications for a <span style='color: white;'>Fortune 500 company</span> as a full-stack software engineer.",
-      "Developed and maintained <span style='color: white;'>scalable backend services</span>, ensuring high availability for critical business applications.",
-      "<span style='color: white;'>Collaborated with a team</span> to design and implement front-end interfaces.",
+      "Seller on partnership with <span style='color: white;'>Amazon</span> on segment of <span style='color: white;'>Games and toys</span>.",
+      "Find New product and increase sales as well as <span style='color: white;'>inventory management and cash</span>, ensuring high availability for critical business applications.",
+      "<span style='color: white;'>Collaborated with a team</span> to design and implement marketing strategies.",
     ],
   },
   {
-    title: "Computer Science",
-    company_name: "ODU",
-    date: "2015 - 2018",
+    title: "External Auditor",
+    company_name: "MoMa",
+    date: "2019 - 2021",
     details: [
-      "Built a <span style='color: white;'>computer science foundation</span> learning theory, computer architecture, and software engineering.",
-      "Worked and interned at <span style='color: white;'>NASA and Norfolk Southern Railway</span> to gain practical experience in the field of data analysis.",
-      "Acted as a member of the <span style='color: white;'>Association for Computing Machinery</span> (ACM).",
+      "Student at <span style='color: white;'>Montpellier Management</span> learning accounting, Legal Compliance, and Risk Management.",
+      "Worked and interned at <span style='color: white;'>Diocèse de Montpellier</span> to gain practical experience in the field of accounting.",
     ],
   },
 ];
@@ -88,4 +73,3 @@ const portfolio = [
 ];
 
 export { experiences, portfolio };
-
