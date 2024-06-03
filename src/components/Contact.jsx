4eb40cafb353bd44bf1fff3f4eadad779a5058c3
FIRST_NAME LAST_NAME <l.frz65@gmail.com>
2024-06-03 04:10:09 +0200

    mail fix

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -21,9 +21,7 @@ const Contact = () => {
   }, [controls]);
 
   return (
-    <div
-      className="md:m-12 md:px-48 flex flex-col sm:flex-row gap-10 overflow-hidden"
-    >
+    <div className="md:m-12 md:px-48 flex flex-col sm:flex-row gap-10 overflow-hidden">
       <motion.div
         initial="hidden"
         animate={controls}
@@ -42,30 +40,30 @@ const Contact = () => {
             },
           },
         }}
-        className='flex-[0.8] md:pb-40 mx-4 sm:mx-auto'
+        className="flex-[0.8] md:pb-40 mx-4 sm:mx-auto"
       >
         <h3 className={styles.sectionText}>Contact</h3>
 
         <form
-          action="https://getform.io/f/8b086558-47d4-49d0-852d-ec8c22da40f7"
+          action="https://getform.io/f/lbjkgdla"
           method="POST"
           className="mt-12 gap-4 flex flex-col"
         >
-          <span className='text-white font-medium mt-3'>Full Name</span>
+          <span className="text-white font-medium mt-3">Full Name</span>
           <input
             type="text"
             name="name"
             placeholder="Enter your full name"
             className="bg-tertiary p-4 text-white border font-medium"
           />
-          <span className='text-white font-medium mt-3'>Email Address</span>
+          <span className="text-white font-medium mt-3">Email Address</span>
           <input
             type="text"
             name="email"
             placeholder="Enter your email address"
             className="bg-tertiary p-4 text-white border font-medium"
           />
-          <span className='text-white font-medium mt-3'>Message</span>
+          <span className="text-white font-medium mt-3">Message</span>
           <textarea
             name="message"
             placeholder="Enter your message"
@@ -73,8 +71,8 @@ const Contact = () => {
             className="bg-tertiary p-4 text-white border font-medium"
           />
           <button
-            type='submit'
-            className='bg-tertiary py-3 px-8 w-fit text-white font-bold shadow-md shadow-primary '
+            type="submit"
+            className="bg-tertiary py-3 px-8 w-fit text-white font-bold shadow-md shadow-primary "
           >
             {loading ? "Sending..." : "Send"}
           </button>
