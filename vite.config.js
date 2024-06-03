6a8105059b27df7cb9d5e4f978b652a160d62382
FIRST_NAME LAST_NAME <l.frz65@gmail.com>
2024-06-03 02:33:28 +0200

    test2

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ["**/*.glb", "**/*.gltf", "**/*.PNG"],
+  assetsInclude: ["**/*.glb", "**/*.gltf", "**/*.png"],
 });
